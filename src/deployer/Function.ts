2bee0b6ead3d22d4b0f9184de688f341f7921cef
Bruce Tan <bruce.tan@serko.com>
2022-11-24 20:01:39 +1300

    Implement storage account

--- src/deployer/Function.ts
+++ src/deployer/Function.ts
@@ -1,21 +1,41 @@
 import { DeploymentConfig } from "./Deployment";
 import { ResourceGroup } from "@pulumi/azure-native/resources";
+import { StorageAccount } from "@pulumi/azure-native/storage";
+import { storage } from "@pulumi/azure-native/types/enums";
 
 export class DisplayrFunction {
-    projectConfig: DeploymentConfig
+    config: DeploymentConfig
     resourceGroup: ResourceGroup
+    storageAccount: StorageAccount
 
     constructor(deploymentConfig: DeploymentConfig){
-        this.projectConfig = deploymentConfig
+        this.config = deploymentConfig
         this.run()
     }
 
     run(){
-        this.resourceGroup = this.deployResourceGroup(this.projectConfig);
+        this.resourceGroup = this.deployResourceGroup(this.config);
+        this.storageAccount = this.deployStorageAccount(this.config, this.resourceGroup)
     }
 
-    deployResourceGroup(deploymentConfig: DeploymentConfig): ResourceGroup {
-        const rgName = `${deploymentConfig.projectName}-${deploymentConfig.environment}-${deploymentConfig.locationCode}-rg`
-        return new ResourceGroup(rgName, {location: deploymentConfig.projectConfig.location})
+    deployResourceGroup(config: DeploymentConfig): ResourceGroup {
+        const rgName = `${config.projectName}-${config.environment}-${config.locationCode}-rg`
+        return new ResourceGroup(rgName, {
+            resourceGroupName: rgName,
+            location: config.projectConfig.location
+        })
+    }
+
+    deployStorageAccount(config: DeploymentConfig, rg: ResourceGroup) : StorageAccount {
+        const stname = `${config.projectName}${config.environment}${config.locationCode}st`
+        return new StorageAccount(stname, {
+            accountName: stname,
+            location: config.projectConfig.location,
+            kind: "StorageV2",
+            resourceGroupName: rg.name,
+            sku: {
+                name: storage.SkuName.Standard_LRS,
+            }
+        })
     }
 }
